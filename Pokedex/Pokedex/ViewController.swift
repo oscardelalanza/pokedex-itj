670fa22cd9ff6b059496e6d2269cbc4acfbe26b3
oscardelalanza <oscardelalanza@gmail.com>
2021-07-24 10:51:51 -0700

    first commit

---
+++ Pokedex/Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Oscar De La Lanza on 24/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
